7f74435202df799cd628c82eaa4068b824390074
Shyam <mohan.shyam@icloud.com>
2016-10-02 20:17:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/article-one', function (req, res) {
-    res.send("Article one requested and will be served.");
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
@@ -15,10 +19,6 @@ app.get('/article-two', function (req, res) {
 
 app.get('/article-three', function (req, res) {
     res.send("Article three requested and will be served.");
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
